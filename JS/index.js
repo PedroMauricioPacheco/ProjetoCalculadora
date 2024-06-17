999fa0f3c4685d835031091b04e0b636917f8b92
Pedro Mauricio Pacheco Neto <101368545+PedroMauricioPacheco@users.noreply.github.com>
2024-06-17 11:22:38 -0300

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -12,13 +12,13 @@ function apagaCampo(campo){         // limpa o input
 function menosCampo(campo){         //apaga o primeiro campo da direita
     var menos = document.getElementById('txt').value
     document.getElementById('txt').value = menos.substring(0, menos.length-1);
-    //atualiza o valor de 'txt' para ser uma substring de 'menos'. Isso essencialmente remove o último caractere do valor do campo.
+    //atualiza o valor de 'txt' para ser uma substring de 'menos'. Isso remove o último caractere do valor do campo.
 }
 function calculo(campo){        //realiza o calculo da input e mostra o resultado 
     var calculo =document.getElementById('txt').value;
     if(calculo){      
-        var resultado = eval(calculo)       //SE 'calculo' tiver valor,usa a função eval() para avaliar a expressão contida em 'calculo' e armazena em 'resultado'
-        document.getElementById('txt').value=resultado      //atualiza o valor do elemento HTML 'txt' para ser igual ao resultado calculado
+        var resultado = eval(calculo)       //SE 'calculo' tiver valor,usa eval() para avaliar a expressão dentro de'calculo' e guarda em 'resultado'
+        document.getElementById('txt').value=resultado      //atualiza o valor de 'txt' para ser igual ao resultado calculado
 
     }
 
